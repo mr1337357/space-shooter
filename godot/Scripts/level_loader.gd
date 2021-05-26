f409cf604fba351ff523cb80a97014275fead9fc
mr1337357 <miss@papaya>
2021-05-26 11:56:33 -0700

    started work on level editing and stuff

---
+++ godot/Scripts/level_loader.gd
@@ -0,0 +1,21 @@
+extends Object
+
+var filename = null
+
+var metadata = {}
+
+var curves = []
+
+var enemies = []
+
+var obstacles = []
+
+func _init(fn = null):
+	filename = fn
+
+func load_from_file():
+	pass
+	
+func save_to_file():
+	pass
+
