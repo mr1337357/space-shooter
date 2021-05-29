c3e10ad6758de3fdd64d81d497ae878f72fd83cb
mr1337357 <miss@papaya>
2021-05-29 13:35:17 -0700

    starting file dialog stuff

--- godot/Scripts/Editor.gd
+++ godot/Scripts/Editor.gd
@@ -7,19 +7,25 @@ extends Node
 var rock_class
 var loader
 var obstacles = []
+var levelpath = ''
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	print('editor ready')
-	rock_class = load("res://Assets/Rock.tscn")
+func load_level():
 	var lvl_load = load("res://scripts/level_loader.gd")
-	loader = lvl_load.new('user://test.lvl')
+	loader = lvl_load.new(levelpath)
 	loader.load_obstacles()
 	for obs in loader.obstacles:
 		var rock = rock_class.instance()
 		rock.translation = Vector3(obs[1],obs[2],obs[3])
 		obstacles.append(rock)
 		add_child(rock)
+
+func list_levels():
+	var asdf
+	
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	rock_class = load("res://Assets/Rock.tscn")
+
 		
 
 
